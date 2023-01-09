de35c2d8d9f6f9bdf2a8e6dd1d06d5211a1c7e3f
Blai Vilarnau Olid <blvilarn@cbr2s4.42barcelona.com>
2023-01-09 19:46:35 +0100

    Printf started - %c %s %% pass paco (not -s)

--- cursus/common_core_1/ft_printf/ft_printf.c
+++ cursus/common_core_1/ft_printf/ft_printf.c
@@ -3,23 +3,60 @@
 /*                                                        :::      ::::::::   */
 /*   ft_printf.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: blvilarn <blvilarn@student.42barcel>       +#+  +:+       +#+        */
+/*   By: blvilarn <blvilarn@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/13 17:33:29 by blvilarn          #+#    #+#             */
-/*   Updated: 2022/12/19 15:46:43 by blvilarn         ###   ########.fr       */
+/*   Updated: 2023/01/09 19:33:46 by blvilarn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include <stdarg.h>
+#include "ft_printf.h"
+
+int	print_content(const char *str, va_list ptr, int *i, int *len)
+{
+	if (str[*i] == 'c')
+		return (ft_putchar(va_arg(ptr, int), i, len));
+	if (str[*i] == 's')
+		return (ft_putstr(va_arg(ptr, char *), i, len));
+	if (str[*i] == 'p')
+		return (-1);
+	if (str[*i] == 'd' || str[*i] == 'i')
+		return (-1);
+	if (str[*i] == 'u')
+		return (-1);
+	if (str[*i] == 'x')
+		return (-1);
+	if (str[*i] == 'X')
+		return (-1);
+	if (str[*i] == '%')
+		return (ft_putchar('%', i, len));
+	else
+		return (-1);
+}
 
 int	ft_printf(const char *str, ...)
 {
-}
-int main(int argc, char **argv)
-{
-	if (argc == 520 && argv != NULL)
-		return (0);
-	ft_printf(NULL, "jdlfjs", "hehe", "m'agraden les pedres", "ueeu", "42");
-	return (0);
+	va_list	ptr;
+	int		i;
+	int		len;
+
+	va_start(ptr, str);
+	i = 0;
+	len = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] != '%')
+		{
+			write(1, &str[i], 1);
+			len++;
+			i++;
+		}
+		else
+		{
+			i++;
+			if (print_content(str, ptr, &i, &len) == -1)
+				return (-1);
+		}
+	}
+	return (len);
 }
---
+++ cursus/common_core_1/ft_printf/utils.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: blvilarn <blvilarn@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/09 16:59:02 by blvilarn          #+#    #+#             */
+/*   Updated: 2023/01/09 19:41:22 by blvilarn         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+int	ft_putchar(char c, int *i, int *len)
+{
+	(*i)++;
+	(*len)++;
+	return (write(1, &c, 1));
+}
+
+int	ft_putstr(char *s, int *i, int *len)
+{
+	int	j;
+
+	(*i)++;
+	if (!s)
+	{
+		(*len) += 6;
+		return (write(1, "(null)", 6));
+	}
+	j = 0;
+	while (s[j] != '\0')
+	{
+		if (write(1, &s[j], sizeof(char)) != 1)
+			return (-1);
+		i++;
+		(*len)++;
+		j++;
+	}
+	return (0);
+}
