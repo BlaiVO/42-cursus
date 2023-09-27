84dbbb93279ce139ed2cb41db35b90b5b10f5e70
BlaiVO <blaivilarnauolid@gmail.com>
2023-09-27 17:45:53 +0200

    Hi there, hello

---
+++ common_core_3/philosophers/philosophers.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosophers.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: blvilarn <blvilarn@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/26 12:22:14 by blvilarn          #+#    #+#             */
+/*   Updated: 2023/09/27 17:44:40 by blvilarn         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <pthread.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/time.h>
+
+typedef struct s_params
+{
+	int	n;
+	int	ttd;
+	int	tte;
+	int	tts;
+	int	eat_limit;
+}	t_params;
+
+//(not) libft
+int	ft_iswhitespace(int c);
+int	ft_isnum(char *str);
