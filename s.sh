ca30a2575db72c5b1d11fe5571ccd228c7fede57
BlaiVO <blaivilarnauolid@gmail.com>
2023-10-13 17:55:21 +0200

    .

--- s.sh
+++ s.sh
@@ -1,11 +1,11 @@
 git filter-branch --env-filter '
-if [[ "$GIT_COMMITTER_EMAIL" = *nuferron* ]]
+if [[ "$GIT_COMMITTER_EMAIL" = *blvilarn* ]]
 then
-    export GIT_COMMITTER_EMAIL="nuriaf207@gmail.com"
+    export GIT_COMMITTER_EMAIL="blaivilarnauolid@gmail.com"
 fi
-if [[ "$GIT_AUTHOR_EMAIL" = *nuferron* ]]
+if [[ "$GIT_AUTHOR_EMAIL" = *blvilarn* ]]
 then
-    export GIT_AUTHOR_EMAIL="nuriaf207@gmail.com"
+    export GIT_AUTHOR_EMAIL="blvilarnauolid@gmail.com"
 fi
 ' -- --all
 git push --force
