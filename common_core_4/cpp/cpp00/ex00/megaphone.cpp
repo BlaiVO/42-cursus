ebf0cbbd268bd9b510c02963c04e4b55feac3512
BlaiVO <blaivilarnauolid@gmail.com>
2024-05-14 16:43:06 +0200

    Now it's actually ready for eval

--- common_core_4/cpp/cpp00/ex00/megaphone.cpp
+++ common_core_4/cpp/cpp00/ex00/megaphone.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
+#include <string>
 
 int main(int argc, char ** argv) {
+	std::string loud_text;
 	if (argc == 1)
 	{
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
@@ -8,10 +10,10 @@ int main(int argc, char ** argv) {
 
 		for (int i = 1; i < argc; i++) {
 			for (size_t j = 0; j < strlen(argv[i]); j++) {
-				putchar(toupper(argv[i][j]));
+				loud_text += toupper(argv[i][j]);
 			}
 		}
 	}
-	std::cout << std::endl;
+	std::cout << loud_text << std::endl;
 	return 0;
 }
--- common_core_4/cpp/cpp00/ex01/Contact.hpp
+++ common_core_4/cpp/cpp00/ex01/Contact.hpp
@@ -20,7 +20,7 @@ class Contact {
 		void		set_phone(std::string new_value);
 		void		set_secret(std::string new_value);
 		void		set_index(int new_vaue);
-		std::string	get_f_nam	e(void); 
+		std::string	get_f_name(void); 
 		std::string	get_l_name(void);
 		std::string	get_n_name(void);
 		std::string	get_phone(void);
