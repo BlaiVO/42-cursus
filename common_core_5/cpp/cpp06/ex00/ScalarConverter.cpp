7e7a665897b857af15adcb30269465b8c8136808
Blai Vilarnau Olid <blaivilarnauolid@gmail.com>
2025-04-15 19:41:07 +0200

    getType seems to be working properly

--- common_core_5/cpp/cpp06/ex00/ScalarConverter.cpp
+++ common_core_5/cpp/cpp06/ex00/ScalarConverter.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <cstdlib>
+#include <climits>
 #include "ScalarConverter.hpp"
 
 ScalarConverter::ScalarConverter(){
@@ -15,27 +16,38 @@ ScalarConverter &ScalarConverter::operator= (const ScalarConverter &a) {
 	return *this;
 }
 
-ScalarConverter::~ScalarConverter(){
-}
-
-int getIntNum(std::string num)
-{
-	if (num.length() == 1 && !isnumber(num[0]))
-		return int(num[0]);
-	return 0;
-}
 
 char getType(std::string num)
 {
-	if (num.length() == 1 && !isnumber(num[0]))
+	std::string literals[6] = {"-inff", "+inff", "nanf", "-inf", "+inf", "nan"};
+	for (int i = 0; i < 6; i++){
+		if (num == literals[i])
+			return 'l';
+	}
+	if (num.length() == 1 && !isdigit(num[0]))
 		return 'c';
-	else if (std::atoi(num.c_str()) != 0 || num == "0")
+	char *p;
+	long converted_int = strtol(num.c_str(), &p, 10);
+	if (!*p && converted_int >= INT_MIN && converted_int <= INT_MAX)
 		return 'i';
-	else if (std::atof(num.c_str()) )
-		//AAAAAAA
+	char *p2;
+	double converted_double = strtod(num.c_str(), &p2);
+	if (num.find('.') == std::string::npos)
+		goto undefined;
+	if (!*p2)
+		return 'd';
+	else if (*p2 == 'f' && (converted_double == ((double)((float)converted_double))))
+	{
+		if (!p2[1])
+			return ('f');
+	}
+	undefined:
+		{
+			return ('u');
+		}
 }
 
 void	ScalarConverter::convert(std::string num)
 {
-	std::cout << getIntNum(num) << std::endl;
+	std::cout << getType(num) << std::endl;
 }
