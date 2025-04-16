54032fa59e7c513efcbf20a69a5acaa9823d1ef0
Blai Vilarnau Olid <blaivilarnauolid@gmail.com>
2025-04-16 21:17:02 +0200

    ex00 seems to be working fine

--- common_core_5/cpp/cpp06/ex00/ScalarConverter.cpp
+++ common_core_5/cpp/cpp06/ex00/ScalarConverter.cpp
@@ -2,6 +2,8 @@
 #include <string>
 #include <cstdlib>
 #include <climits>
+#include <iomanip>
+#include <cmath>
 #include "ScalarConverter.hpp"
 
 ScalarConverter::ScalarConverter(){
@@ -16,6 +18,23 @@ ScalarConverter &ScalarConverter::operator= (const ScalarConverter &a) {
 	return *this;
 }
 
+void casePseudoLiteral(std::string num)
+{
+	std::string f_literals[3] = {"-inff", "+inff", "nanf"};
+	for (int i = 0; i < 3; i++)
+	{
+		if (num == f_literals[i])
+		{
+			num.erase(num.end()-1);
+			break;
+		}
+	}
+	std::cout << "char: impossible" << std::endl;
+	std::cout << "int: impossible" << std::endl;
+	std::cout << "float: " << num << 'f' << std::endl;
+	std::cout << "double: " << num << std::endl;
+}
+
 void caseChar(std::string num)
 {
 	char c = num[0];
@@ -23,9 +42,9 @@ void caseChar(std::string num)
 		std::cout << "char: " << c << std::endl;
 	else
 		std::cout << "char: Non displayable" << std::endl;
-	std::cout << "int: " << (int)c << std::endl;
-	std::cout << "float: " << (int)c << ".0f" << std::endl;
-	std::cout << "double: " << (int)c << ".0" << std::endl;
+	std::cout << "int: " << static_cast<int>(c) << std::endl;
+	std::cout << "float: " << std::fixed << std::setprecision(1) << static_cast<float>(c) << 'f' << std::endl;
+	std::cout << "double: " << static_cast<double>(static_cast<double>(c)) << std::endl;
 }
 
 void caseInt(std::string num)
@@ -42,44 +61,94 @@ void caseInt(std::string num)
 	std::cout << std::endl;
 
 	std::cout << "int: " << i << std::endl;
-	std::cout << "float: " << i << ".0f" << std::endl;
-	std::cout << "double: " << i << ".0" << std::endl;
+	std::cout << "float: " << std::fixed << std::setprecision(1) << static_cast<float>(i) << 'f' << std::endl;
+	std::cout << "double: " << static_cast<double>(i) << std::endl;
+
 }
 
 void caseFloat(std::string num)
 {
-	int f = static_cast<float>(strtod(num.c_str(), NULL));
+	float f = static_cast<float>(strtod(num.c_str(), NULL));
 	char c = static_cast<char>(f);
 	if (isprint(c))
 		std::cout << "char: " << c << std::endl;
 	else
 		std::cout << "char: Non displayable" << std::endl;
-	std::cout << "int: " << static_cast<int>(f) << std::endl;
-	std::cout << "float: " << f << std::endl;
-	std::cout << "double: " << f << std::endl;
+	char *p;
+	long int_check = std::strtol(num.c_str(), &p, 10);
+	if (*p != '.' || int_check < INT_MIN || int_check > INT_MAX)
+		std::cout << "int: invalid int" << std::endl;
+	else
+		std::cout << "int: " << static_cast<int>(int_check) << std::endl;
+
+	double integral;
+	if (std::modf(f, &integral) == 0)
+	{
+		std::cout << "float: " << std::fixed << std::setprecision(1) << f << 'f' << std::endl;
+		std::cout << "double: " << f << std::endl;
+	}
+	else
+	{
+		std::cout.precision(7);
+		std::cout << "float: " << f << 'f' << std::endl;
+		std::cout.precision(7);
+		std::cout << "double: " << f << std::endl;
+	}
 }
 
-void getType(std::string num)
+void caseDouble(std::string num)
+{
+	double d = strtod(num.c_str(), NULL);
+	char c = static_cast<char>(d);
+	if (isprint(c))
+		std::cout << "char: " << c << std::endl;
+	else
+		std::cout << "char: Non displayable" << std::endl;
+
+	char *p;
+	long int_check = std::strtol(num.c_str(), &p, 10);
+	if (*p != '.' || int_check < INT_MIN || int_check > INT_MAX)
+		std::cout << "int: invalid int" << std::endl;
+	else
+		std::cout << "int: " << static_cast<int>(d) << std::endl;
+
+	double integral;
+	if (std::modf(d, &integral) == 0)
+	{
+		std::cout << "float: " << std::fixed << std::setprecision(1) << d << 'f' << std::endl;
+		std::cout << "double: " << d << std::endl;
+	}
+	else
+	{
+		std::cout.precision(7);
+		std::cout << "float: " << static_cast<float>(d) << 'f' << std::endl;
+		std::cout.precision(16);
+		std::cout << "double: " << d << std::endl;
+	}
+}
+
+void	ScalarConverter::convert(std::string num)
 {
 	std::string pseudo_literals[6] = {"-inff", "+inff", "nanf", "-inf", "+inf", "nan"};
 	for (int i = 0; i < 6; i++){
 		if (num == pseudo_literals[i])
-			return ; //pseudo-literal
+			return casePseudoLiteral(num);
 	}
 	if (num.length() == 1 && !isdigit(num[0]) && isprint(num[0]))
 		return caseChar(num);
 	char *p;
-	long converted_int = strtol(num.c_str(), &p, 10);
-	if (!*p && converted_int >= INT_MIN && converted_int <= INT_MAX)
+	long i = strtol(num.c_str(), &p, 10);
+	if (!*p && i >= INT_MIN && i <= INT_MAX)
 		return caseInt(num);
 	char *p2;
-	double converted_double = strtod(num.c_str(), &p2);
+	strtod(num.c_str(), &p2);
 	if (num.find('.') == std::string::npos || num.find('.') == num.length() - 1)
 		goto undefined;
 	if (!*p2)
-		return; // double
-	else if (*p2 == 'f' && (converted_double == ((double)((float)converted_double))))
+		return caseDouble(num);
+	else if (*p2 == 'f' && isdigit(num[num.find('f') - 1]))
 	{
+
 		if (!p2[1])
 			return caseFloat(num);
 	}
@@ -87,9 +156,4 @@ void getType(std::string num)
 		{
 			std::cout << "Error: invalid input" << std::endl;
 		}
-}
-
-void	ScalarConverter::convert(std::string num)
-{
-	getType(num);
 }
