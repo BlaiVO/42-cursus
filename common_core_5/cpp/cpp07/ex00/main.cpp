e402ce25f94fd049d9605bcb333df6913670d1a1
Blai Vilarnau <blai8@Blais-MacBook-Air.local>
2025-04-17 19:35:02 +0200

    added subject tests

--- common_core_5/cpp/cpp07/ex00/main.cpp
+++ common_core_5/cpp/cpp07/ex00/main.cpp
@@ -4,6 +4,21 @@
 int main()
 {
     {
+        std::cout << "----Subject tests----" << std::endl;
+        int a = 2;
+        int b = 3;
+        ::swap( a, b );
+        std::cout << "a = " << a << ", b = " << b << std::endl;
+        std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+        std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+        std::string c = "chaine1";
+        std::string d = "chaine2";
+        ::swap(c, d);
+        std::cout << "c = " << c << ", d = " << d << std::endl;
+        std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+        std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+    }
+    {
         int a = 8;
         int b = 6;
 
@@ -11,7 +26,7 @@ int main()
 
         std::cout << "a, b: " << a << ", " << b << std::endl;
         std::cout << "(swap)" << std::endl;
-        swap(a, b);
+        ::swap(a, b);
         std::cout << "a, b: " << a << ", " << b << std::endl;
         
         std::cout << "Min: " << min(a, b) << std::endl;
@@ -25,7 +40,7 @@ int main()
 
         std::cout << "a, b: " << a << ", " << b << std::endl;
         std::cout << "(swap)" << std::endl;
-        swap(a, b);
+        ::swap(a, b);
         std::cout << "a, b: " << a << ", " << b << std::endl;
         
         std::cout << "Min: " << min(a, b) << std::endl;
@@ -35,11 +50,11 @@ int main()
         char a = 'r';
         char b = 'h';
 
-        std::cout << "----String tests----" << std::endl;
+        std::cout << "----Char tests----" << std::endl;
 
         std::cout << "a, b: " << a << ", " << b << std::endl;
         std::cout << "(swap)" << std::endl;
-        swap(a, b);
+        ::swap(a, b);
         std::cout << "a, b: " << a << ", " << b << std::endl;
         
         std::cout << "Min: " << min(a, b) << std::endl;
