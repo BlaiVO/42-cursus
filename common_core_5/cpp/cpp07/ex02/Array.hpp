c0fe3d6a7a1ad98c38d8f83739b1f4b95f384618
BlaiVO <blaivilarnauolid@gmail.com>
2025-04-17 23:30:10 +0200

    this s*** does not compile

--- common_core_5/cpp/cpp07/ex02/Array.hpp
+++ common_core_5/cpp/cpp07/ex02/Array.hpp
@@ -1,16 +1,22 @@
 #ifndef ARRAY_HPP
 # define ARRAY_HPP
 
+# include <stdexcept>
+
 template<class T>
 class Array
 {
     private:
+        T *_array;
+        unsigned int _size;
     public:
         Array();
-        Array(unsigned int);
+        Array(unsigned int n);
         Array(const Array &copy);
         Array &operator=(const Array &a);
+        T &operator[](int i);
         ~Array();
+        unsigned int size() const;
 };
 
 #endif
---
+++ common_core_5/cpp/cpp07/ex02/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "Array.hpp"
+
+int main()
+{
+    Array <int> a(5);
+    a[1] = 1;
+    a[2] = 2;
+    return 0;
+}
