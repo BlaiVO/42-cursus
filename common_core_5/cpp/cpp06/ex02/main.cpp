19a8ffcee033c56d9472dc1353470b9ff8ce9fcd
BlaiVO <blaivilarnauolid@gmail.com>
2025-04-17 16:10:42 +0200

    ex02 (and by definition, cpp06) finished

---
+++ common_core_5/cpp/cpp06/ex02/main.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <cstdlib>
+
+#include "A.hpp"
+#include "B.hpp"
+#include "C.hpp"
+
+Base *generate(void)
+{
+    srand(time(0));
+    switch (rand() % 3)
+    {
+        case 0:
+            return new A();
+            break;
+        case 1:
+            return new B();
+            break;
+        case 2:
+            return new C();
+            break;
+    }
+    return (NULL);
+}
+
+void identify(Base *p)
+{
+    if (dynamic_cast<A*>(p))
+        std::cout << "Object is type A" << std::endl;
+    else if (dynamic_cast<B*>(p))
+        std::cout << "Object is type B" << std::endl;
+    else if (dynamic_cast<C*>(p))
+        std::cout << "Object is type C" << std::endl;
+    else
+        std::cout << "Object is not A, B or C" << std::endl;
+}
+
+void identify(Base &p)
+{
+    try {
+        (void) dynamic_cast<A&>(p);
+        std::cout << "Object is type A" << std::endl;
+    }
+    catch (...) {
+        try {
+            (void) dynamic_cast<B&>(p);
+            std::cout << "Object is type B" << std::endl;
+        }
+        catch (...) {
+            try {
+                (void) dynamic_cast<C&>(p);
+                std::cout << "Object is type C" << std::endl;
+            }
+            catch (...) {
+                std::cout << "Object is not A, B or C" << std::endl;
+            }
+        }
+    }
+}
+
+int main()
+{
+    Base *base = generate();
+    identify(base);
+    identify(*base);
+
+    return 0;
+}
