8ae57ae267a122c3dd3d5cb2287f349c9b691029
Blai Vilarnau Olid <blaivilarnauolid@gmail.com>
2025-04-18 18:58:29 +0200

    ready for eval (?

--- common_core_5/cpp/cpp07/ex02/Array.hpp
+++ common_core_5/cpp/cpp07/ex02/Array.hpp
@@ -10,13 +10,55 @@ class Array
         T *_array;
         unsigned int _size;
     public:
-        Array();
-        Array(unsigned int n);
-        Array(const Array &copy);
-        Array &operator=(const Array &a);
-        T &operator[](int i);
-        ~Array();
-        unsigned int size() const;
+        Array(){
+            this->_array = (T)NULL;
+            this->_size = 0;
+        }
+
+        Array(unsigned int n){
+            this->_array = new T[n]();
+            this->_size = n;
+        }
+
+        Array(const Array &copy){
+            this->_size =copy.size();
+            this->_array = new T[this->_size]();
+            for (unsigned int i = 0; i < copy.size(); i++)
+            {
+                this->_array[i] = copy[i];
+            }
+        }
+
+        Array &operator=(const Array &a){
+            if (this != &a)
+            {
+                if (this->_array)
+                    delete [] this->_array;
+                this->_size = a.size();
+                this->_array = new T[this->_size]();
+                for (unsigned int i = 0; i < a.size(); i++)
+                {
+                    this->_array[i] = a[i];
+                }
+            }
+            return *this;
+        }
+
+        T &operator[](int i) const {
+            if (i < 0 || i >= (int)this->_size)
+                throw std::out_of_range("Index out of bonds");
+            return this->_array[i];
+        }
+
+        ~Array(){
+            if (this->_array)
+                delete [] this->_array;
+        }
+
+        unsigned int size() const
+        {
+            return this->_size;
+        }
 };
 
 #endif
--- common_core_5/cpp/cpp07/ex02/main.cpp
+++ common_core_5/cpp/cpp07/ex02/main.cpp
@@ -1,10 +1,35 @@
 #include <iostream>
+#include <string>
 #include "Array.hpp"
 
 int main()
 {
-    Array <int> a(5);
-    a[1] = 1;
-    a[2] = 2;
+    {
+        std::cout << "-----Int check-----" << std::endl;
+        Array <int> a(5);
+        for (unsigned int i = 0; i < a.size(); i++)
+            a[i] = i + 1;
+        std::cout << a[3] << std::endl;
+        Array <int> b = a;
+        std::cout << "B size: " << b.size() << std::endl;
+        for (unsigned int i = 0; i < b.size(); i++)
+        std::cout << b[i] << std::endl;
+        try
+        {
+            std::cout << b[28] << std::endl;
+        }
+        catch (std::exception &e)
+        {
+            std::cout << "nope: " << e.what() << std::endl;
+        }
+    }
+    {
+        std::cout << "-----String check-----" << std::endl;
+        Array <std::string> s(3);
+        s[0] = "I ";
+        s[1] = "like ";
+        s[2] = "Rocks";
+        std::cout << s[0] << s[1] << s[2] << std::endl;
+    }
     return 0;
 }
